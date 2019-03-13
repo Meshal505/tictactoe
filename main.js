20249886abdf1c8089e4a06891f7b77909ff0a08
Meshal505 <rings-3@hotmail.com>
2019-03-13 11:22:44 +0300

    The Game is ready now

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 //Store Player moves
 let player1 = [];
 let player2 = [];
@@ -35,14 +36,20 @@ let winCombo = [
 
 // }
 function resetGame (){
-    $("button").on("click", function(){
-        $(".square").removeAttr("style");
+    $(".square").each( function(){
+            $(this).removeAttr("style");
+            console.log(this)
         whoWins = false
         player1 =[];
         player2 =[];
+        current_player = 0;
     });
-}
 
+}
+$("button").on("click", function(){
+
+    resetGame();
+})
 
 
 function changePlayer(){
@@ -56,47 +63,43 @@ function changePlayer(){
 
 
 $(".square").on("click", function(){ //when click on any zone1
-    let id = $(this).attr("id") //get the attribute of id of particular square clicked and store in variable id
-   
+    console.log(player1.length)
     
-    if(current_player == 0){
+    let id = $(this).attr("id") //get the attribute of id of particular square clicked and store in variable id
+            if(current_player == 0){
                             if (whoWins == false){
                             if ($(this).attr("style") == undefined || $(this).attr("style") == "undefined"){ 
-          
-                             $(this).css("background-image", "url('https://i.ytimg.com/vi/tRx9TCKJWSs/maxresdefault.jpg')").css("background-size","100px 100px")
+                                $(this).css("background-image", "url('https://i.ytimg.com/vi/tRx9TCKJWSs/maxresdefault.jpg')").css("background-size","100px 100px")
                               player1.push(parseInt(id));
-                       
-                        
-                        
-       
-        changePlayer() } }
+                              console.log(id);
+                              
+                             
+                             changePlayer() } }
+                             if (player1.length == 4) { 
+                                console.log("hi")
+                              resetGame();
+                          }
 
     }
     else if (current_player ==1){
                          if (whoWins == false) {
-                         if ($(this).attr("style") == undefined) {
+                         if ($(this).attr("style") == undefined || $(this).attr("style") == "undefined") {
                               $(this).css("background-image", "url('http://pluspng.com/img-png/circle-png-circle-png-hd-1600.png')").css("background-size","100px 100px")
                         player2.push(parseInt(id));
         
-        changePlayer() } }
-    }
+                                changePlayer() } } }
+                             
     
     if (whoWins !== true){
         checkWhoWins(player1,player2)
         
     }
-    
-    
-    
+    });
 
-   
-   
-    
-});
-// i removed these two params and is still running fine so why Ebre told me to put them.
 function checkWhoWins (player1,player2){ 
                 for ( var i = 0; i < winCombo.length ; i++ ){
-                     
+                       
+                        
                         if (player1.sort().join('') == winCombo[i].join('')){
                             
                             whoWins = true;
